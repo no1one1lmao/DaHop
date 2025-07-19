2a251a16570862e90a0ea75855504680e1bb09b7
no1one1lmao <bocharvlad4@gmail.com>
2025-07-20 00:02:54 +0300

    Update script.lua

--- script.lua
+++ script.lua
@@ -1,3 +1,7 @@
+if queue_on_teleport then
+     queue_on_teleport('loadstring(game:HttpGet("https://raw.githubusercontent.com/no1one1lmao/DaHop/refs/heads/main/script.lua"))()')
+end
+
 local ScreenGui = Instance.new("ScreenGui")
 local Draggable = Instance.new("Frame")
 local Main = Instance.new("Frame")
